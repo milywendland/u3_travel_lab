2f27587ff6f89c378db2e9329e53399835be25ff
milywendland <milynicwendland@gmail.com>
2022-04-06 08:18:59 -0600

    fixed postreducer

--- travel-list/client/src/App.js
+++ travel-list/client/src/App.js
@@ -1,12 +1,11 @@
 import logo from './logo.svg'
 import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import Posts from './componenets/Posts'
 
 function App() {
   return (
     <div className="App">
-      <h2>Travel Blog</h2>
+      <Posts />
     </div>
   )
 }
