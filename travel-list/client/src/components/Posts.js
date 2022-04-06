53cce22ccb523021f1b6c0e5ecdcdcf1478fdda5
milywendland <milynicwendland@gmail.com>
2022-04-06 08:03:21 -0600

    set up post component

--- travel-list/client/src/components/Posts.js
+++ travel-list/client/src/components/Posts.js
@@ -1,9 +1,33 @@
 import { useEffect } from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
 
-const Posts = () => {
-  return <div>Posts</div>
+const mapStateToProps = ({ postState }) => {
+  return { postState }
 }
 
-export default Posts
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchPosts: () => dispatch(LoadPosts())
+  }
+}
+
+const Posts = (props) => {
+  useEffect(() => {
+    props.fetchPosts()
+  }, [])
+
+  return (
+    <div>
+      <h2>Travel Blog</h2>
+      {props.postState.posts.map((post) => {
+        ;<div key={post.id}>
+          <h3>{post.name}</h3>
+          <img src={post.image} alt={post.name} />
+          <p>{post.decsription}</p>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Posts)
